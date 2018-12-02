3e5fbfd02a708ce53d78244cac46e052250f044f
azelsky <andry.zelinskiy@gmail.com>
2018-12-02 20:20:13 +0200

    forms part 2 database

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from firstapp.models import Pizza
 
-
 from firstapp.forms import OrderForm
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def home(request):
@@ -12,10 +13,17 @@ def home(request):
 
 def pizza_detail(request, pizza_id):
     pizza = get_object_or_404(Pizza, id=pizza_id)
-    form = OrderForm(initial={
+    form = OrderForm(request.POST or None, initial={
         'pizza': pizza
     })
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('{}?sent=True'.format(reverse('pizza_detail', kwargs={'pizza_id': pizza_id})))
+
     return render(request, 'pizza_detail.html', {
         'pizza': pizza,
-        'form': form
+        'form': form,
+        'sent': request.GET.get('sent', False)
     })
